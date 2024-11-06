ff728e0ea6d859f63cf26c4a0ff08e38188ee708
cielaton <vuwin24680@gmail.com>
2024-11-07 00:21:40 +0700

    [Application] Added the Home and Alert screens

---
+++ src/screens/Alert.tsx
@@ -0,0 +1,13 @@
+
+import { Text, View } from "@tamagui/core";
+import React from "react";
+
+const AlertScreen = () => {
+  return (
+    <View>
+      <Text fontSize={20} fontWeight={100}>This is the alert screen</Text>
+    </View>
+  )
+}
+
+export default AlertScreen
---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "@tamagui/core";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text fontSize={20} fontWeight={100}>This is the home screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
