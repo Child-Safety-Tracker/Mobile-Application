0e31e9930b664dbb0524ec55b3674260ab93c990
cielaton <vuwin24680@gmail.com>
2024-11-09 00:17:54 +0700

    [Home] Implemented and separated the MapBox's map into different component.

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import Mapbox from '@rnmapbox/maps';
 
 import {dark} from '@lib/color.ts';
 
-// @ts-ignore
-import {OPEN_MAP_PUBLIC_KEY} from '@env';
-
 import HomeHeading from './components/Home.Heading';
-
-Mapbox.setAccessToken(OPEN_MAP_PUBLIC_KEY);
+import HomeMap from './components/Home.Map';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <HomeHeading />
-      <View
-        style={{
-          flex: 0.55,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Mapbox.MapView
-          style={{
-            width: '100%',
-            aspectRatio: 1,
-            overflow: 'hidden',
-            borderRadius: 20,
-          }}
-        />
-      </View>
+      <HomeMap />
     </View>
   );
 };
---
+++ src/screens/home/components/Home.Map.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import Mapbox from '@rnmapbox/maps';
+
+// @ts-ignore
+import {OPEN_MAP_PUBLIC_KEY} from '@env';
+
+Mapbox.setAccessToken(OPEN_MAP_PUBLIC_KEY);
+
+const HomeMap = () => {
+  return (
+    <View style={styles.container}>
+      <Mapbox.MapView style={styles.map} />
+    </View>
+  );
+};
+
+export default HomeMap;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.55,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  map: {
+    width: '100%',
+    aspectRatio: 1,
+    overflow: 'hidden',
+    borderRadius: 20,
+  },
+});
