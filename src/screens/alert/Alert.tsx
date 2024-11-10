8b8d84ac5f7ce260fac20ffa8556074105bc039e
cielaton <vuwin24680@gmail.com>
2024-11-10 16:57:43 +0700

    [Alert] Completed the configuration UI.

--- src/screens/alert/Alert.tsx
+++ src/screens/alert/Alert.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {dark} from '@lib/colors/theme';
 import AlertHeading from './components/Alert.Heading';
 import AlertConfiguration from './components/Alert.Configuration';
+
+// @ts-ignore
+import FlagFillIcon from '@assets/icons/screens/alert/flag-fill.svg';
+// @ts-ignore
+import BoundaryLineIcon from '@assets/icons/screens/alert/boundary-line.svg';
+
+const ReferenceIcon = () => (
+  <FlagFillIcon width={20} height={20} fill={dark.colors.text.hex} />
+);
+
+const BoundaryIcon = () => (
+  <BoundaryLineIcon width={20} height={20} fill={dark.colors.text.hex} />
+);
 
 const AlertScreen = () => {
   return (
@@ -11,9 +24,19 @@ const AlertScreen = () => {
         <AlertHeading />
       </View>
       <View style={styles.alertConfigurationWrapper}>
-        <AlertConfiguration />
+        <AlertConfiguration
+          icon={<ReferenceIcon />}
+          configName={'Reference'}
+          configValue1={'16°05\'09.8"N'}
+          configValue2={'108°09\'04.5"E'}
+        />
         <View style={styles.alertConfigurationSeparator} />
-        <AlertConfiguration />
+        <AlertConfiguration
+          icon={<BoundaryIcon />}
+          configName={'Boundary'}
+          configValue1={'100m'}
+          configValue2={''}
+        />
       </View>
     </View>
   );
@@ -34,7 +57,7 @@ const styles = StyleSheet.create({
   },
 
   alertConfigurationWrapper: {
-    flex: 0.15,
+    flex: 0.13,
     flexDirection: 'row',
   },
 
--- src/screens/alert/components/Alert.Configuration.tsx
+++ src/screens/alert/components/Alert.Configuration.tsx
@@ -9,18 +9,30 @@ import FlagFillIcon from '@assets/icons/screens/alert/flag-fill.svg';
 // @ts-ignore
 import ArrowRightIcon from '@assets/icons/screens/alert/arrow-right-line.svg';
 
-const AlertConfiguration = () => {
+const AlertConfiguration = ({
+  icon,
+  configName,
+  configValue1,
+  configValue2,
+}: {
+  icon: any;
+  configName: string;
+  configValue1: string;
+  configValue2: string;
+}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.componentsWrapper}>
+      <View style={styles.headingComponentsWrapper}>
         <View style={styles.headingWrapper}>
-          <FlagFillIcon width={20} height={20} fill={dark.colors.text.hex} />
-          <Text style={styles.heading}>Reference</Text>
+          {icon}
+          <Text style={styles.heading}>{configName}</Text>
         </View>
         <ArrowRightIcon width={20} height={20} fill={dark.colors.text.hex} />
       </View>
-      <Text style={styles.info}>16°05'09.8"N</Text>
-      <Text style={styles.info}>108°09'04.5"E</Text>
+      <View>
+        <Text style={styles.info}>{configValue1}</Text>
+        <Text style={styles.info}>{configValue2}</Text>
+      </View>
     </View>
   );
 };
@@ -30,13 +42,15 @@ export default AlertConfiguration;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-between',
     borderRadius: 15,
-    padding: 10,
+    padding: 12,
     backgroundColor: dark.colors.base.hex,
   },
 
-  componentsWrapper: {
+  headingComponentsWrapper: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 
   headingWrapper: {
@@ -44,6 +58,7 @@ const styles = StyleSheet.create({
   },
 
   heading: {
+    marginLeft: 8,
     fontSize: fontSize.text.medium,
     color: dark.colors.text.hex,
   },
