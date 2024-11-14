1ae1a8d7adbb00cfb24ff77c36da8a0c8e9344ef
cielaton <vuwin24680@gmail.com>
2024-11-14 09:36:04 +0700

    [Alert] Implemented the drop down selection from react-native-element-dropdown.

--- src/screens/alert/components/Alert.DeviceSelection.tsx
+++ src/screens/alert/components/Alert.DeviceSelection.tsx
@@ -1,102 +1,65 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
+import {Dropdown} from 'react-native-element-dropdown';
+
+import AlertDevice from './Alert.Device';
 
 import {dark} from '@lib/colors/theme';
-import {fontSize} from '@lib/fontSize';
 
-// @ts-ignore
-import MapPinIconLine from '@assets/icons/screens/home/map-pin-line.svg';
-// @ts-ignore
-import ArrowDownFill from '@assets/icons/screens/alert/arrow-down-fill.svg';
+import MapIconLine from '@assets/icons/screens/alert/map-pin-line.svg';
 
 const devices = [
   {
-    label: 'Option 1',
-    value: 'option1',
+    label: 'Device 1',
+    value: 'Device 1',
   },
   {
-    label: 'Option 2',
-    value: 'option2',
+    label: 'Device 2',
+    value: 'Device 2',
   },
   {
-    label: 'Option 3',
-    value: 'option3',
-  },
-  {
-    label: 'Option 4',
-    value: 'option4',
+    label: 'Device 3',
+    value: 'Device 3',
   },
 ];
 
 const AlertDeviceSelection = () => {
-  const [selectedDevice, setSelectedDevice] = useState();
+  const [value, setValue] = React.useState('');
+  const [isFocused, setIsFocused] = React.useState();
 
   return (
-    <View style={styles.container}>
-      <View style={styles.locationInfoContainer}>
-        <View style={styles.mapPinIconContainer}>
-          <MapPinIconLine
-            width={20}
-            height={20}
-            color={dark.colors.green.hex}
-          />
-        </View>
-        <View style={styles.locationInfo}>
-          <Text style={styles.deviceName}>{'device'}</Text>
-          <Text style={styles.deviceLocation}>Location</Text>
-        </View>
-      </View>
-      <ArrowDownFill
-        borderWidth={5}
-        width={30}
-        height={30}
-        color={dark.colors.text.hex}
-      />
-    </View>
+    <Dropdown
+      style={{
+        backgroundColor: dark.colors.base.hex,
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        flex: 0.5,
+      }}
+      containerStyle={{
+        borderWidth: 0,
+        borderRadius: 10,
+        backgroundColor: dark.colors.surface0.hex,
+      }}
+      itemContainerStyle={{
+        borderRadius: 10,
+      }}
+      itemTextStyle={{
+        paddingLeft: 10,
+        color: dark.colors.text.hex,
+      }}
+      activeColor={dark.colors.base.hex}
+      selectedTextStyle={{paddingLeft: 10, color: dark.colors.text.hex}}
+      placeholderStyle={{paddingLeft: 10, color: dark.colors.text.hex}}
+      data={devices}
+      value={devices[0]}
+      labelField={'label'}
+      valueField={'value'}
+      renderLeftIcon={() => (
+        <MapIconLine width={20} height={20} fill={dark.colors.green.hex} />
+      )}
+      onChange={item => setValue(item.value)}
+    />
   );
 };
 
 export default AlertDeviceSelection;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: dark.colors.base.hex,
-    borderRadius: 10,
-    padding: 12,
-    paddingRight: 20,
-  },
-
-  locationInfoContainer: {
-    flexDirection: 'row',
-  },
-
-  mapPinIconContainer: {
-    padding: 12,
-    backgroundColor: dark.colors.mantle.hex,
-    borderRadius: 10,
-  },
-
-  locationInfo: {
-    marginLeft: 10,
-    fontSize: fontSize.text.medium,
-  },
-
-  deviceName: {
-    color: dark.colors.text.hex,
-  },
-
-  deviceLocation: {
-    color: dark.colors.text.hex,
-    opacity: 0.5,
-  },
-
-  lastUpdate: {
-    marginTop: 5,
-    fontSize: fontSize.text.medium,
-    color: dark.colors.text.hex,
-    opacity: 0.5,
-  },
-});
