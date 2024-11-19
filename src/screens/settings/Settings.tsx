f0f4d37b25352021afb19d04184896b899bb14f2
cielaton <vuwin24680@gmail.com>
2024-11-19 22:01:46 +0700

    [Settings] Added the notification configuration, temporary done for the setting screen.

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Switch} from 'react-native';
 
 import {dark} from '@lib/colors/theme';
 import {fontSize} from '../../../lib/fontSize';
@@ -10,10 +10,14 @@ import Separator from '../../components/Separator';
 
 // @ts-ignore
 import EarthIconLine from '@assets/icons/screens/settings/earth-line.svg';
+// @ts-ignore
 import PencilIcon from '@assets/icons/screens/settings/pencil-fill.svg';
+// @ts-ignore
 import UserIcon from '@assets/icons/screens/settings/user-fill.svg';
+import NotificationIcon from '@assets/icons/screens/settings/notification-fill.svg';
 
 const SettingScreen = () => {
+  const [isEnabled, setIsEnabled] = React.useState(false);
   return (
     <View style={styles.container}>
       <View style={styles.userInfoWrapper}>
@@ -26,6 +30,44 @@ const SettingScreen = () => {
           }
           label={'Language'}
           value={'English'}
+        />
+        <Separator height={15} />
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <Text
+            style={{
+              color: dark.colors.text.hex,
+              fontSize: fontSize.heading.small,
+              lineHeight: fontSize.heading.small,
+              fontWeight: 'bold',
+            }}>
+            Notification
+          </Text>
+          <Switch
+            value={isEnabled}
+            trackColor={{
+              false: dark.colors.surface0.hex,
+              true: dark.colors.surface1.hex,
+            }}
+            thumbColor={isEnabled ? dark.colors.teal.hex : dark.colors.text.hex}
+            onValueChange={() => setIsEnabled(!isEnabled)}
+          />
+        </View>
+        <Separator height={15} />
+        <SettingsOption
+          icon={
+            <NotificationIcon
+              width={20}
+              height={20}
+              fill={dark.colors.text.hex}
+            />
+          }
+          label={'Customization'}
+          value={'Phone notification'}
         />
         <Separator height={15} />
         <Text
@@ -51,7 +93,6 @@ const SettingScreen = () => {
           label={'Email'}
           value={'user@gmail.com'}
         />
-        <Separator height={20} />
       </View>
     </View>
   );
