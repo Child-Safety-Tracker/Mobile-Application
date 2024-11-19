3c669fab9c0d5743ff74545c2843d45a73eb6c35
cielaton <vuwin24680@gmail.com>
2024-11-19 21:20:17 +0700

    [Settings] Clean up the settings option file.

--- src/screens/settings/components/Settings.Option.tsx
+++ src/screens/settings/components/Settings.Option.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import {dark} from '../../../../lib/colors/theme';
 import {fontSize} from '../../../../lib/fontSize';
@@ -8,50 +8,53 @@ import EarthIconLine from '@assets/icons/screens/settings/earth-line.svg';
 
 const SettingsOption = ({label, value}: {label: string; value: string}) => {
   return (
-    <TouchableOpacity
-      style={{
-        height: 55,
-        paddingLeft: (55 - 35) / 2, // Equal padding for top, bottom and left of the inner icon
-        borderRadius: 10,
-        backgroundColor: dark.colors.base.hex,
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}>
-      <View
-        style={{
-          height: 35,
-          aspectRatio: 1,
-          borderRadius: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: dark.colors.mantle.hex,
-        }}>
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.iconContainer}>
         <EarthIconLine width={20} height={20} fill={dark.colors.text.hex} />
       </View>
-      <View
-        style={{
-          paddingLeft: 10,
-          justifyContent: 'center',
-        }}>
-        <Text
-          style={{
-            color: dark.colors.text.hex,
-            fontSize: fontSize.text.medium,
-            lineHeight: fontSize.text.medium,
-          }}>
-          {label}
-        </Text>
-        <Text
-          style={{
-            color: dark.colors.text.hex,
-            fontSize: fontSize.text.smallMedium,
-            opacity: 0.5,
-          }}>
-          {value}
-        </Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.value}>{value}</Text>
       </View>
     </TouchableOpacity>
   );
 };
 
 export default SettingsOption;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 55,
+    paddingLeft: (55 - 35) / 2, // Equal padding for top, bottom and left of the inner icon
+    borderRadius: 10,
+    backgroundColor: dark.colors.base.hex,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  iconContainer: {
+    height: 35,
+    aspectRatio: 1,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: dark.colors.mantle.hex,
+  },
+
+  textContainer: {
+    paddingLeft: 10,
+    justifyContent: 'center',
+  },
+
+  label: {
+    color: dark.colors.text.hex,
+    fontSize: fontSize.text.medium,
+    lineHeight: fontSize.text.medium,
+  },
+
+  value: {
+    color: dark.colors.text.hex,
+    fontSize: fontSize.text.smallMedium,
+    opacity: 0.5,
+  },
+});
