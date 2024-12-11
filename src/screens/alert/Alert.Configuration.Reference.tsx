896387cf3b27bf059f8544b211e43f0efc3e45b7
cielaton <vuwin24680@gmail.com>
2024-12-11 17:01:40 +0700

    [Alert] Added the ability to pick a point on the map for reference

--- src/screens/alert/Alert.Configuration.Reference.tsx
+++ src/screens/alert/Alert.Configuration.Reference.tsx
@@ -1,15 +1,18 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import Mapbox, {Camera} from '@rnmapbox/maps';
+import Mapbox, {Camera, PointAnnotation} from '@rnmapbox/maps';
 
 // @ts-ignore
 import {OPEN_MAP_PUBLIC_KEY} from '@env';
 import {LocationContext} from '../../context/Location.context';
+import {Position} from '@rnmapbox/maps/lib/typescript/src/types/Position';
 
 Mapbox.setAccessToken(OPEN_MAP_PUBLIC_KEY);
 
 const AlertConfigurationReference = ({route}: any) => {
+  const [isPressed, setIsPressed] = useState(false);
   const {location, isLoadingLocation}: any = useContext(LocationContext);
+  const [pressedCoordinates, setPressedCoordinates] = useState([]);
 
   // Get the passed prop
   const {selectedDevice}: any = route.params;
@@ -24,14 +27,29 @@ const AlertConfigurationReference = ({route}: any) => {
     ];
   }
 
+  // @ts-ignore
+  // @ts-ignore
   return (
     <View style={styles.container}>
       <Mapbox.MapView
+        onPress={(event: any) => {
+          const {geometry} = event;
+          setIsPressed(true);
+          setPressedCoordinates(geometry.coordinates);
+        }}
         style={styles.map}
         scaleBarEnabled={false}
         logoEnabled={false}
         compassEnabled={false}
         attributionEnabled={false}>
+        {isPressed ? (
+          // Ignore the complaint about missing children prop to render the default annotation
+          // @ts-ignore
+          <PointAnnotation
+            id={'Pressed point'}
+            coordinate={pressedCoordinates!}
+          />
+        ) : null}
         <Camera
           centerCoordinate={coordinates}
           animationMode={'none'}
