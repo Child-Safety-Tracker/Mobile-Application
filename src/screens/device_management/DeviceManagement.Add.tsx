3e3a6faf472a14cbd1ffa583562427a369036a1e
cielaton <vuwin24680@gmail.com>
2024-12-14 14:17:09 +0700

    [Device Management] Added the camera opener for the app, enable the ability to scan QR code.

--- src/screens/device_management/DeviceManagement.Add.tsx
+++ src/screens/device_management/DeviceManagement.Add.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {
+  useCameraDevice,
+  useCameraPermission,
+  Camera,
+} from 'react-native-vision-camera';
 
 const DeviceManagementAdd = () => {
-  return (
-    <View>
-      <Text>This is the device add screen</Text>
+  // Check for camera permission
+  const {hasPermission, requestPermission} = useCameraPermission();
+  if (!hasPermission) {
+    console.log('[DeviceManagement] No camera permission');
+  }
+
+  const camera = useCameraDevice('back');
+  if (camera == null) {
+    console.log('[DeviceManagement] No camera device');
+  }
+
+  return camera == null ? null : (
+    <View style={styles.container}>
+      <Camera style={styles.cameraView} device={camera} isActive={true} />
     </View>
   );
 };
 
 export default DeviceManagementAdd;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  cameraView: {
+    flex: 1,
+  },
+});
