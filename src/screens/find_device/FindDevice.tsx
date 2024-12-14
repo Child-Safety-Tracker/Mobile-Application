3db79b0007d91bae95d3e67ca210b15d2c42cf5c
cielaton <vuwin24680@gmail.com>
2024-12-14 19:14:38 +0700

    [App] Temporarily done for today

--- src/screens/find_device/FindDevice.tsx
+++ src/screens/find_device/FindDevice.tsx
@@ -5,6 +5,9 @@ import {dark} from '@lib/colors/theme';
 import FindDeviceHeading from './components/FindDevice.Heading';
 import FindDeviceSelection from './components/FindDevice.Selection';
 
+// @ts-ignore
+import UserLocationIcon from '@assets/icons/screens/find_device/circle-dot.svg';
+
 const FindDeviceScreen = () => {
   return (
     <View style={styles.container}>
@@ -13,6 +16,9 @@ const FindDeviceScreen = () => {
       </View>
       <View style={styles.findDeviceSelectionWrapper}>
         <FindDeviceSelection />
+      </View>
+      <View style={styles.findDeviceIndicatorWrapper}>
+        <UserLocationIcon width={30} height={30} color={dark.colors.teal.hex} />
       </View>
     </View>
   );
@@ -34,5 +40,11 @@ const styles = StyleSheet.create({
 
   findDeviceSelectionWrapper: {
     flex: 0.12,
+  },
+
+  findDeviceIndicatorWrapper: {
+    flex: 0.48,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
