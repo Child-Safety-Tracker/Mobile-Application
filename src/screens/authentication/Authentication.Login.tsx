539924509f16c62e8235355f39654a35bfdd1dcf
cielaton <vuwin24680@gmail.com>
2024-12-14 18:17:53 +0700

    [Authentication] Added the register button and finalized the styling

--- src/screens/authentication/Authentication.Login.tsx
+++ src/screens/authentication/Authentication.Login.tsx
@@ -20,26 +20,8 @@ const InformationInput = ({description}: {description: string}) => {
   const [text, setText] = useState('');
   return (
     <View>
-      <Text
-        style={{
-          marginLeft: 2,
-          fontSize: fontSize.text.mediumLarge,
-          color: 'white',
-        }}>
-        {description}
-      </Text>
-      <TextInput
-        style={{
-          marginTop: 15,
-          paddingHorizontal: 10,
-          height: 55,
-          borderRadius: 13,
-          backgroundColor: dark.colors.surface0.hex,
-          color: dark.colors.text.hex,
-        }}
-        value={text}
-        onChangeText={setText}
-      />
+      <Text style={styles.inputDescription}>{description}</Text>
+      <TextInput style={styles.input} value={text} onChangeText={setText} />
     </View>
   );
 };
@@ -55,65 +37,25 @@ const Separator = ({height}: {height: number}) => {
 };
 
 const OAuthButton = ({icon}: {icon: any}) => {
-  return (
-    <TouchableOpacity
-      style={{
-        height: 55,
-        borderRadius: 13,
-        borderWidth: 1,
-        borderColor: dark.colors.subtext0.hex,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      {icon}
-    </TouchableOpacity>
-  );
+  return <TouchableOpacity style={styles.OAuthButton}>{icon}</TouchableOpacity>;
 };
 
 const AuthenticationLogin = () => {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flex: 0.1,
-          justifyContent: 'center',
-        }}>
-        <Text
-          style={{
-            textAlignVertical: 'center',
-            fontSize: fontSize.heading.large,
-            fontWeight: 'bold',
-            color: dark.colors.text.hex,
-          }}>
-          Login
-        </Text>
+      <View style={styles.headingContainer}>
+        <Text style={styles.header}>Login</Text>
       </View>
-      <View
-        style={{
-          flex: 0.3,
-        }}>
+      <View style={styles.inputContainer}>
         <InformationInput description={'Username'} />
         <Separator height={20} />
         <InformationInput description={'Password'} />
         <Separator height={30} />
-        <TouchableOpacity
-          style={{
-            height: 55,
-            borderRadius: 13,
-            backgroundColor: dark.colors.teal.hex,
-            justifyContent: 'center',
-          }}>
-          <Text
-            style={{
-              textAlign: 'center',
-              fontSize: fontSize.text.mediumLarge,
-              fontWeight: 500,
-              color: dark.colors.crust.hex,
-            }}>
-            Submit
-          </Text>
+        <TouchableOpacity style={styles.submitContainer}>
+          <Text style={styles.submitText}>Submit</Text>
         </TouchableOpacity>
-        <Separator height={30} />
+      </View>
+      <View style={styles.OAuthContainer}>
         <OAuthButton
           icon={
             <GoogleIcon width={35} height={35} color={dark.colors.teal.hex} />
@@ -126,6 +68,9 @@ const AuthenticationLogin = () => {
           }
         />
       </View>
+      <TouchableOpacity style={styles.registerContainer}>
+        <Text style={styles.registerText}>Register</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -138,5 +83,75 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 20,
     backgroundColor: dark.colors.crust.hex,
+  },
+
+  headingContainer: {
+    flex: 0.1,
+    justifyContent: 'center',
+  },
+  header: {
+    textAlignVertical: 'center',
+    fontSize: fontSize.heading.large,
+    fontWeight: 'bold',
+    color: dark.colors.text.hex,
+  },
+
+  inputContainer: {
+    flex: 0.4,
+  },
+
+  inputDescription: {
+    marginLeft: 2,
+    fontSize: fontSize.text.mediumLarge,
+    color: 'white',
+  },
+
+  input: {
+    marginTop: 15,
+    paddingHorizontal: 10,
+    height: 55,
+    borderRadius: 13,
+    backgroundColor: dark.colors.surface0.hex,
+    color: dark.colors.text.hex,
+  },
+
+  submitContainer: {
+    height: 55,
+    borderRadius: 13,
+    backgroundColor: dark.colors.teal.hex,
+    justifyContent: 'center',
+  },
+
+  submitText: {
+    textAlign: 'center',
+    fontSize: fontSize.text.mediumLarge,
+    fontWeight: 500,
+    color: dark.colors.crust.hex,
+  },
+
+  OAuthContainer: {
+    flex: 0.2,
+  },
+
+  OAuthButton: {
+    height: 55,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: dark.colors.subtext0.hex,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  registerContainer: {
+    flex: 0.1,
+    justifyContent: 'flex-start',
+  },
+
+  registerText: {
+    textDecorationLine: 'underline',
+    textDecorationColor: dark.colors.teal.hex,
+    fontSize: fontSize.text.mediumLarge,
+    color: dark.colors.text.hex,
+    textAlign: 'center',
   },
 });
