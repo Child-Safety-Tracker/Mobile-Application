a08152d63687e7757c94bc3af3f330b251c4dcb0
cielaton <vuwin24680@gmail.com>
2024-12-14 18:46:00 +0700

    [Find Device] Added the header along with button to toggle the feature

--- src/screens/find_device/FindDevice.tsx
+++ src/screens/find_device/FindDevice.tsx
@@ -2,9 +2,14 @@ import {View, StyleSheet} from 'react-native';
 import React from 'react';
 
 import {dark} from '@lib/colors/theme';
+import FindDeviceHeading from './components/FindDevice.Heading';
 
 const FindDeviceScreen = () => {
-  return <View style={styles.container} />;
+  return <View style={styles.container}>
+    <View style={styles.headingWrapper}>
+      <FindDeviceHeading />
+    </View>
+  </View>;
 };
 
 export default FindDeviceScreen;
@@ -12,6 +17,12 @@ export default FindDeviceScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
     backgroundColor: dark.colors.crust.hex,
   },
+
+  headingWrapper: {
+    flex: 0.1
+  }
 });
---
+++ src/screens/find_device/components/FindDevice.Heading.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Switch} from 'react-native';
+
+// @ts-ignore
+import {fontSize} from '@lib/fontSize';
+import {dark} from '@lib/colors/theme';
+
+const FindDeviceHeading = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Find Device</Text>
+      <Switch
+        value={isEnabled}
+        trackColor={{
+          false: dark.colors.surface0.hex,
+          true: dark.colors.surface1.hex,
+        }}
+        thumbColor={isEnabled ? dark.colors.teal.hex : dark.colors.text.hex}
+        onValueChange={() => {
+          setIsEnabled(!isEnabled);
+        }}
+      />
+    </View>
+  );
+};
+
+export default FindDeviceHeading;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  welcome: {
+    fontSize: fontSize.heading.large,
+    fontWeight: 'bold',
+    color: dark.colors.text.hex,
+  },
+
+  userName: {
+    color: dark.colors.teal.hex,
+  },
+
+  notificationContainer: {
+    padding: 10,
+    borderRadius: '50%',
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: dark.colors.surface0.hex,
+    backgroundColor: dark.colors.mantle.hex,
+  },
+});
