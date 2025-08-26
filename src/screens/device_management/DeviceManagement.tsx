705b6e53210d50c370db1c030313de56fa3c90cb
lukas0173 <vuwin24680@gmail.com>
2025-08-26 15:30:14 +0700

    [Device Management] Added an empty screen when there is no device

--- src/screens/device_management/DeviceManagement.tsx
+++ src/screens/device_management/DeviceManagement.tsx
@@ -8,95 +8,107 @@ import {dark} from '@lib/colors/theme';
 import {deviceColors} from '@lib/colors/device';
 import {DeviceContext} from '../../context/Device.context';
 import {useNavigation} from '@react-navigation/native';
+import EmptyDevice from "../../components/EmptyDevice";
 
 const deviceInfo = [
-  {
-    id: 1,
-    deviceName: 'Device 1',
-  },
-  {
-    id: 2,
-    deviceName: 'Device 2',
-  },
-  {
-    id: 3,
-    deviceName: 'Device 3',
-  },
-  {
-    id: 4,
-    deviceName: 'Device 4',
-  },
-  {
-    id: 5,
-    deviceName: 'Device 5',
-  },
+    {
+        id: 1,
+        deviceName: 'Device 1',
+    },
+    {
+        id: 2,
+        deviceName: 'Device 2',
+    },
+    {
+        id: 3,
+        deviceName: 'Device 3',
+    },
+    {
+        id: 4,
+        deviceName: 'Device 4',
+    },
+    {
+        id: 5,
+        deviceName: 'Device 5',
+    },
 ];
 
-const Separator = () => <View style={{height: 15}} />;
+const Separator = () => <View style={{height: 15}}/>;
 
 const DeviceManagementScreen = () => {
-  const navigation = useNavigation();
-  const {device}: any = useContext(DeviceContext);
-  return (
-    <View style={styles.container}>
-      <View style={styles.deviceManagementHeadingWrapper}>
-        <DeviceManagementHeading />
-      </View>
-      <View
-        style={{
-          flex: 0.9,
-        }}>
-        <FlatList
-          data={device}
-          showsVerticalScrollIndicator={false}
-          initialScrollIndex={0}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={Separator}
-          renderItem={({item, index}) => (
-            <Device
-              deviceName={deviceInfo[index].deviceName}
-              deviceColor={Object.values(deviceColors)[index]}
-              index={index}
-            />
-          )}
-        />
-      </View>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('DeviceManagementAdd' as never);
-        }}
-        style={{
-          bottom: 20,
-          height: 50,
-          alignItems: 'center',
-          justifyContent: 'center',
-          alignSelf: 'center',
-          aspectRatio: 1,
-          borderRadius: '100%',
-          backgroundColor: dark.colors.teal.hex,
-        }}>
-        <Text
-          style={{
-            color: dark.colors.crust.hex,
-          }}>
-          +
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+    const navigation = useNavigation();
+    const {device}: any = useContext(DeviceContext);
+
+    if (!!device.length)
+        return (
+            <View style={styles.container}>
+                <View style={styles.deviceManagementHeadingWrapper}>
+                    <DeviceManagementHeading/>
+                </View>
+                <View
+                    style={{
+                        flex: 0.9,
+                    }}>
+                    <FlatList
+                        data={device}
+                        showsVerticalScrollIndicator={false}
+                        initialScrollIndex={0}
+                        keyExtractor={(item, index) => index.toString()}
+                        ItemSeparatorComponent={Separator}
+                        renderItem={({item, index}) => (
+                            <Device
+                                deviceName={deviceInfo[index].deviceName}
+                                deviceColor={Object.values(deviceColors)[index]}
+                                index={index}
+                            />
+                        )}
+                    />
+                </View>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('DeviceManagementAdd' as never);
+                    }}
+                    style={{
+                        bottom: 20,
+                        height: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        alignSelf: 'center',
+                        aspectRatio: 1,
+                        borderRadius: '100%',
+                        backgroundColor: dark.colors.teal.hex,
+                    }}>
+                    <Text
+                        style={{
+                            color: dark.colors.crust.hex,
+                        }}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    else
+        return (
+            <View style={styles.container}>
+                <View style={styles.deviceManagementHeadingWrapper}>
+                    <DeviceManagementHeading/>
+                </View>
+                <EmptyDevice/>
+            </View>
+        )
 };
 
 export default DeviceManagementScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    paddingTop: 20,
-    backgroundColor: dark.colors.crust.hex,
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingTop: 20,
+        backgroundColor: dark.colors.crust.hex,
+    },
 
-  deviceManagementHeadingWrapper: {
-    flex: 0.1,
-  },
+    deviceManagementHeadingWrapper: {
+        flex: 0.1,
+    },
 });
